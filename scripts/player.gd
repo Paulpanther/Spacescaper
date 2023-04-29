8c78f762bdccfc0a2ba5f8f97a99a0d20fd60afc
IcePyro <louis.haub@googlemail.com>
2023-04-29 16:50:26 +0200

    rework movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,19 @@
 extends RigidBody3D
+@export var linear_thruster_strength = 1.0	
+@export var rotational_thruster_strength = 1.0
 
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-func _input(event):
-	if event.is_action_pressed("forwards"):
-		apply_central_impulse(Vector3(0, 0, 1) * -transform.basis.z)
-	elif event.is_action_pressed("backwards"):
-		apply_central_impulse(Vector3(0, 0, -1) * -transform.basis.z)
-	elif event.is_action_pressed("rot_left"):
-		apply_torque_impulse(Vector3(0, 0.1, 0))
-	elif event.is_action_pressed("rot_right"):
-		apply_torque_impulse(Vector3(0, -0.1, 0))
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
+
+func _physics_process(delta):
+	var linear = Vector3(
+		(Input.get_action_strength("right") - Input.get_action_strength("left")) * delta * linear_thruster_strength,
+		(Input.get_action_strength("up") - Input.get_action_strength("down")) * delta * linear_thruster_strength,
+		(Input.get_action_strength("backward") - Input.get_action_strength("forward")) * delta * linear_thruster_strength,
+	)
+	
+	#(Input.get_action_strength("roll_left") - Input.get_action_strength("roll_right")) * delta * rotational_thruster_strength
+	
+	apply_central_impulse(linear)
+	apply_torque_impulse(transform.basis.y * (Input.get_action_strength("yaw_left") - Input.get_action_strength("yaw_right")) * delta * rotational_thruster_strength)
+	apply_torque_impulse(transform.basis.x * (Input.get_action_strength("pitch_down") - Input.get_action_strength("pitch_up")) * delta * rotational_thruster_strength)
